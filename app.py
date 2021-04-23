7aec8ee2ca16f8bacd9839871b92dc8844a4d3b6
Isaac Wallace <iwallace96@gmail.com>
2021-04-23 05:40:46 -0400

    turn in

--- app.py
+++ app.py
@@ -1,1 +1,82 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, request, jsonify
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+
+# app.config['SECRET_KEY'] = "abcdef"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+@app.route("/")
+def root():
+    """Homepade"""
+    
+    return render_template("index.html")
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+    """Return all cupcakes in table"""
+    
+    cupcakes = [cupcake.to_dict() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Add cupcake, and return data about new"""
+    data = request.json
+    cupcake=Cupcake(
+        flavor=data['flavor'],
+        rating=data['rating'],
+        size=data['size'],
+        image=data['image'] or None )
+    
+    db.session.add(cupcake)
+    db.session.commit()
+    
+    #POST requests should return HTTP status
+    return (jsonify(cupcake=cupcake.to_dict()), 201)
+
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake(cupcake_id):
+    """return data on specific cupcake"""
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake.to_dict())
+
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """Update cupcake from data in request. Return updated data.
+
+    Returns JSON like:
+        {cupcake: [{id, flavor, rating, size, image}]}
+    """
+
+    data = request.json
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    cupcake.flavor = data['flavor']
+    cupcake.rating = data['rating']
+    cupcake.size = data['size']
+    cupcake.image = data['image']
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.to_dict())
+
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["DELETE"])
+def remove_cupcake(cupcake_id):
+    """delete cupcake and return confirmation"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="Deleted")
+
--- models.py
+++ models.py
@@ -1,1 +1,41 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+default_image = "https://tinyurl.com/demo-cupcake"
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """cupcakes profile"""
+    
+    __tablename__ = "cupcake"
+    
+    
+    id = db.Column(db.Integer,
+                primary_key=True,
+                autoincrement=True)
+    flavor = db.Column(db.Text,
+                       nullable=False)
+    size = db.Column(db.Text,
+                       nullable=False)
+    rating = db.Column(db.Float,
+                       nullable=False)
+    image = db.Column(db.Text,
+                      nullable=False, default=default_image)
+    
+    def to_dict(self):
+        """Serialize cupcake to a dict of cupcake info."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "rating": self.rating,
+            "size": self.size,
+            "image": self.image,
+        }
